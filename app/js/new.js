75564549f9eefc62a98a6f49b6c98002a682bc98
mKarnovan <karnovan95@mail.ru>
2017-05-16 18:00:04 +0300

    all done

--- app/js/new.js
+++ app/js/new.js
@@ -1,7 +1,14 @@
-function NewsCtrl ($scope) {
-	$scope.news = [
-	{text: 'Title', description:'Description'}, 
-	]; 
+function NewsCtrl ($scope, $http) {
+	$scope.news = []; 
+	$http.get('http://localhost:3000/api/news')
+	.success((request) => {
+		for (var i=0; i<request.data.length; i++) {
+			var title = request.data[i].title;
+			var desc = request.data[i].description;
+			var id = request.data[i].id;
+			$scope.news.push({id: id, text: title, description: desc});
+		}
+	})
 
 	$scope.getTotalTodos = function () {
 		return $scope.news.length;
@@ -16,13 +23,43 @@ function NewsCtrl ($scope) {
 			alert("Description is empty");
 			return;
 		}
-
+		var data = {
+			id_user: 1,
+			title: $scope.fromTodoTitle,
+			description: $scope.fromTodoDescription
+		};
+		var postItems = JSON.stringify(data);
+		$http.post('http://localhost:3000/api/news', postItems);
 		$scope.news.push({text:$scope.fromTodoTitle, description: $scope.fromTodoDescription});
 		$scope.fromTodoTitle = '';
 		$scope.fromTodoDescription = '';
 	}
 
-    $scope.remove = function(index) {
-    	$scope.news.splice(index, 1);
-    };
+	$scope.remove = function(index) {
+		var _id = $scope.news[index].id;
+		$http.delete('http://localhost:3000/api/news/' + _id);
+		$scope.news.splice(index, 1);
+	};
 }
+
+const myApp = angular.module('loginApp', []);
+
+myApp.controller('NewsUser', function ($scope, $http) {
+
+	$scope.addUser = function () {
+		if(!$scope.userLogin){
+			alert("Login is empty");
+			return;
+		}
+		if(!$scope.userPassword ) {
+			alert("password is empty");
+			return;
+		}
+		var data = {
+			login: $scope.userLogin,
+			password: $scope.userPassword
+		};
+		var postItems = JSON.stringify(data);
+		$http.post('http://localhost:3000/api/users', postItems);
+	}
+});
---
+++ myproj/routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+var db = require('../queries');
+
+router.get('/api/news', db.getAllNews);
+router.get('/api/news/:id', db.getSingleNews);
+router.post('/api/news', db.createNews);
+router.post('/api/users', db.addUser);
+router.put('/api/news/:id', db.updateNews);
+router.delete('/api/news/:id', db.removeNews);
+
+
+module.exports = router;
